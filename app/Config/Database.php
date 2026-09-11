88ea154ac694137dd635d0b5315e76545aab533c
Muhammad Fathur Rohman <hyungkun.co@gmail.com>
2026-09-11 23:56:56 +0700

    feat: switch database to MySQL localhost (port 3307), clean runtime artifacts, and add .gitignore

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,21 +27,21 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
+        'username' => 'root',
         'password' => '',
-        'database' => '',
+        'database' => 'tekape_db',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
         'DBDebug'  => true,
-        'charset'  => 'utf8',
-        'DBCollat' => 'utf8_general_ci',
+        'charset'  => 'utf8mb4',
+        'DBCollat' => 'utf8mb4_unicode_ci',
         'swapPre'  => '',
         'encrypt'  => false,
         'compress' => false,
         'strictOn' => false,
         'failover' => [],
-        'port'     => 3306,
+        'port'     => 3307,
     ];
 
     /**
